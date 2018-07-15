1622fb1a82d39ca2d25ac251b89d15d1beb46f19
fengxiaomao <fxm@autoforce.net>
2018-07-15 21:39:19 +0800

    make 'deferred' objects into a class

--- index.js
+++ index.js
@@ -32,12 +32,7 @@ function Promise(fn) {
 
   this.then = function(onFulfilled, onRejected) {
     return new Promise(function(resolve, reject) {
-      handle({
-        onFulfilled: onFulfilled,
-        onRejected: onRejected,
-        resolve: resolve,
-        reject: reject
-      })
+      handle(new Handler(onFulfilled, onRejected, resolve, reject))
     })
   }
 
@@ -48,7 +43,7 @@ function Promise(fn) {
     }
     nextTick(function() {
       var cb = state ? deferred.onFulfilled : deferred.onRejected
-      if (typeof cb !== 'function') {
+      if (cb === null) {
         (state ? deferred.resolve : deferred.reject)
         return
       }
@@ -124,3 +119,9 @@ try {
   reject(e)
 }
 
+function Handler(onFulfilled, onRejected, resolve, reject) {
+  this.onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : null;
+  this.onRejected = typeof onRejected === 'function' ? onRejected : null;
+  this.resolve = resolve
+  this.reject = reject
+}
